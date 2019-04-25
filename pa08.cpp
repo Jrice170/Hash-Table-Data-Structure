049cbaedcb1c8e6c22415a0a8a848954e17b4979
Dr. Taylor <taylor@mst.ed>
2019-04-24 22:32:45 -0500

    formatting and style

--- pa08.cpp
+++ pa08.cpp
@@ -50,6 +50,10 @@ int main()
         cout << e.what();
     }
 
+    MyUnorderedMap<std::string, int> virus_tree;
+    // get_virus_frequency(virus_tree);
+    // virus_tree.print();
+
     return 0;
 }
 
